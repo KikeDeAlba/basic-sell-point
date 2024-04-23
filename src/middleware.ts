bd68423a4dbff99659856c0ae806561d6f0a5a43
KikeDeAlba <likingvirus1@gmail.com>
2024-04-23 02:15:46 -0700

    Refactor onRequest middleware function to handle token-based authentication and redirect to dashboard if token exists

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,13 @@
 import type { MiddlewareHandler } from "astro";
 
-export const onRequest: MiddlewareHandler = async ({ request, cookies, redirect }, next) => {
+export const onRequest: MiddlewareHandler = async ({ request, cookies, redirect, url }, next) => {
     const token = cookies.get('token');
+
+    console.log('token', token)
+
+    if (token != null && token.value != null && url.pathname === '/') {
+        return redirect('/dashboard')
+    }
 
     if (token == null || token.value == null) {
         return redirect('/?error=unauthorized')
