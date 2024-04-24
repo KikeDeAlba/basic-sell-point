477586ac16fcd262ea7733cba69b70b7bd24e7a8
KikeDeAlba <likingvirus1@gmail.com>
2024-04-24 02:03:52 -0700

    Update API endpoints and dashboard pages for inventory management

--- src/pages/api/auth/logout.ts
+++ src/pages/api/auth/logout.ts
@@ -1,7 +1,9 @@
 import type { APIContext } from "astro";
 
 export const GET = async ({ cookies, redirect }: APIContext) => {
-    cookies.delete('token')
-    
+    cookies.delete('token', {
+        path: '/'
+    })
+
     return redirect('/')
 }
---
+++ src/pages/api/inventory/create-product.ts
@@ -0,0 +1,23 @@
+import { db } from "@/services/mysql";
+import type { APIContext } from "astro";
+import { z } from "astro/zod";
+import { products } from "db/schema";
+
+const ProductSchema = z.object({
+    name: z.coerce.string(),
+    description: z.coerce.string(),
+    price: z.coerce.number(),
+    stock: z.coerce.number()
+})
+
+export const POST = async ({ request, redirect }: APIContext) => {
+    const data = await request.formData()
+
+    const json = Object.fromEntries(data.entries())
+
+    const product = ProductSchema.parse(json)
+
+    await db.insert(products).values(product).execute()
+
+    return redirect('/dashboard/inventory')
+}
