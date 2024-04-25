5a376dec71912b946dacf5595771a36d34149058
KikeDeAlba <likingvirus1@gmail.com>
2024-04-25 12:27:45 -0700

    Update database configuration to use environment variables

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,15 +1,17 @@
 import 'dotenv/config';
 import type { Config } from 'drizzle-kit';
+import { config } from 'dotenv';
+config();
 
 export default {
   schema: './db/schema.ts',
   out: './drizzle',
   driver: 'mysql2', // 'pg' | 'mysql2' | 'better-sqlite' | 'libsql' | 'turso'
   dbCredentials: {
-    host: '127.0.0.1',
-    user: 'root',
-    password: '123456',
-    database: 'sell_point'
+    host: process.env.DB_HOST || '',
+    user: process.env.DB_USER || '',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || ''
   },
   verbose: false
 } satisfies Config;
