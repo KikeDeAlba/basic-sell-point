e8d390b1a177f28dfdd6b84d6c59321fceb19068
KikeDeAlba <likingvirus1@gmail.com>
2024-04-25 12:28:50 -0700

    Update database configuration to use environment variables

--- src/services/mysql.ts
+++ src/services/mysql.ts
@@ -2,10 +2,10 @@ import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
 
 const poolConnection = await mysql.createPool({
-  host: "127.0.0.1",
-  user: "root",
-  database: "sell_point",
-  password: "123456"
+  host: process.env.DB_HOST || '',
+  user: process.env.DB_USER || '',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME || ''
 })
 
 export const db = drizzle(poolConnection);
