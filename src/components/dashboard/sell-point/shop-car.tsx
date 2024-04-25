1af2be6a3fabf1f1ad59afef31d24b3596a4ac38
KikeDeAlba <likingvirus1@gmail.com>
2024-04-25 15:31:33 -0700

    Refactor shop-car.tsx to improve layout and add checkout button

--- src/components/dashboard/sell-point/shop-car.tsx
+++ src/components/dashboard/sell-point/shop-car.tsx
@@ -5,10 +5,22 @@ export const ShopCar = () => {
     const shopCarProducts = useShopCarStore((state) => state.products);
 
     return (
-        <ul>
-            {shopCarProducts.map((product) => (
-                <TicketProduct key={product.id} {...product} />
-            ))}
-        </ul>
+        <section className="flex-1 h-full flex flex-col justify-between">
+            <ul className="flex flex-col gap-2">
+                {shopCarProducts.map((product) => (
+                    <TicketProduct key={product.id} {...product} />
+                ))}
+            </ul>
+
+            <div className="flex">
+                <p className="text-white/80">Total: {
+                    shopCarProducts.reduce((acc, { price, quantity }) => acc + price * quantity, 0)
+                }</p>
+
+                <button className="w-full p-4 bg-blue-500 text-white rounded-md">
+                    Checkout
+                </button>
+            </div>
+        </section>
     )
 }
