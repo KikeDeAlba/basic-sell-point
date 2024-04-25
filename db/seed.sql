40dba9a35bc14a33a2235f4e4513952ca31021ef
Shino <dg047359@gmail.com>
2024-04-25 13:03:17 -0700

    Add new user and worker API endpoints

--- db/seed.sql
+++ db/seed.sql
@@ -1,3 +1,8 @@
--- Active: 1710196960256@@127.0.0.1@3306@sell_point
-INSERT INTO users (first_name, last_name, email, token, age, role) VALUES
-    ('Jesus Enrique', 'De Alba Gaytan', 'jedealbagaytan@gmail.com', '$2b$10$KnyJnOME/iULpCzUJdta8uzARZyJqxz5pU7Ucvl9fvs1s6rCxH516', '20', 'admin')
+-- Active: 1696921476499@@127.0.0.1@3306@sell_point
+INSERT INTO
+    users (
+        first_name, last_name, email, token, age, role
+    )
+VALUES (
+        'Wendy', 'Wendy', 'wendy@gmail.com', '$2b$10$KnyJnOME/iULpCzUJdta8uzARZyJqxz5pU7Ucvl9fvs1s6rCxH516', '20', 'admin'
+    )
---
+++ src/pages/api/workers.ts
@@ -0,0 +1,36 @@
+import type { APIContext } from "astro";
+import { z } from "astro/zod";
+import { db } from "@/services/mysql";
+import { users } from "db/schema";
+import bcrypt from 'bcrypt'
+
+export const WorkerSchema = z.object({
+    firstName: z.coerce.string(),
+    lastName: z.coerce.string(),
+    email: z.coerce.string(),
+    age: z.coerce.number(),
+    role: z.enum(['worker', 'admin']),
+    profilePicture: z.coerce.string()
+})
+
+export const POST = async ({ request, redirect }: APIContext) =>{
+    const data = await request.formData()
+
+    const json = Object.fromEntries(data.entries())
+
+    const password = json.password.toString() ?? ''
+
+    if (password === '') {
+        return redirect('/dashboard/workers?error=Password is required')
+    }
+
+    const hashToken = await bcrypt.hash(password,bcrypt.genSaltSync())
+
+    const worker = WorkerSchema.parse(json)
+
+    await db.insert(users).values({
+        ...worker,
+        token: hashToken
+    }).execute()
+
+}
