4c56cd4c94b6c9782a865624510cdefd8aca67dc
KikeDeAlba <likingvirus1@gmail.com>
2024-04-25 12:33:01 -0700

    Update drizzle.config.ts to remove verbose option

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -12,6 +12,5 @@ export default {
     user: process.env.DB_USER || '',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || ''
-  },
-  verbose: false
+  }
 } satisfies Config;
