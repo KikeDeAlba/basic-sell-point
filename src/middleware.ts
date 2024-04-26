6f5b6607fb4fd41296040c2518eaec1950698217
KikeDeAlba <likingvirus1@gmail.com>
2024-04-26 16:56:28 -0700

    Refactor middleware.ts to add user object to locals and handle redirects

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,7 @@
 import type { MiddlewareHandler } from "astro";
+import { db } from "./services/mysql";
+import { users } from "db/schema";
+import { eq } from "drizzle-orm";
 
 /**
  * Middleware function to handle incoming requests.
@@ -6,7 +9,7 @@ import type { MiddlewareHandler } from "astro";
  * @param {MiddlewareNextFunction} next - The next function to be called in the middleware chain.
  * @returns {Promise<void>} - A promise that resolves when the middleware has finished processing.
  */
-export const onRequest: MiddlewareHandler = async ({ cookies, redirect, url }, next) => {
+export const onRequest: MiddlewareHandler = async ({ cookies, redirect, url, locals }, next) => {
     if (url.pathname.startsWith('/api')) {
         return next();
     }
@@ -14,13 +17,20 @@ export const onRequest: MiddlewareHandler = async ({ cookies, redirect, url }, n
     // Get the value of the 'token' cookie
     const token = cookies.get('token');
 
+    const [user] = await db.select().from(users).where(
+        eq(users.token, token?.value ?? '')
+    )
+
+    // @ts-expect-error - Add the user object to the locals object
+    if (user != null) locals.user = user;
+
     // If a valid token exists and the current URL is '/', redirect to '/dashboard'
-    if (token != null && token.value != null && url.pathname === '/') {
+    if (user != null && url.pathname === '/') {
         return redirect('/dashboard')
     }
 
     // If no token or an invalid token exists, redirect to '/?error=unauthorized'
-    if ((token == null || token.value == null) && url.pathname !== '/') {
+    if (user == null && url.pathname !== '/') {
         return redirect('/?error=unauthorized')
     }
 
