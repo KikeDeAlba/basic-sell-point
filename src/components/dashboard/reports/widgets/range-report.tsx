008d3f54f32f93bc1fa66347d80995fade5951d4
KikeDeAlba <likingvirus1@gmail.com>
2024-04-26 17:59:30 -0700

    Refactor aside.astro to remove unnecessary imports and update comments

---
+++ src/components/dashboard/reports/widgets/range-report.tsx
@@ -0,0 +1,95 @@
+import type { sales } from "db/schema"
+import { useEffect, useState } from "react"
+import { priceFormmater } from "../../sell-point/product-card"
+
+
+const getCurrentDateMinurOne = () => {
+    const date = new Date()
+    date.setDate(date.getDate() - 1)
+    return date
+}
+
+export const RangeReport = () => {
+    const [dates, setDates] = useState({
+        startDate: getCurrentDateMinurOne(),
+        endDate: getCurrentDateMinurOne()
+    })
+
+    const [currentSales, setSales] = useState<typeof sales.$inferSelect[]>([])
+
+    useEffect(() => {
+        fetch('/api/reports/range', {
+            method: 'POST',
+            body: JSON.stringify(dates)
+        })
+            .then(res => {
+                if (res.ok) {
+                    return res.json()
+                }
+            })
+            .then(data => {
+                setSales(data)
+            })
+    }, [dates])
+
+    return (
+        <article
+            className="flex flex-col gap-4 p-4 w-fit shadow-md rounded-md bg-white/30"
+        >
+            <header
+                className="flex justify-between items-center"
+            >
+                <h2 className="text-xl font-semibold">Range Report</h2>
+            </header>
+
+            <div
+                className="flex gap-4 items-center"
+            >
+                <input
+                    type="date"
+                    className="border border-gray-300 rounded-md p-2"
+                    value={dates.startDate.toISOString().split('T')[0]}
+                    onChange={(e) => {
+                        setDates({
+                            ...dates,
+                            startDate: new Date(e.target.value)
+                        })
+                    }}
+                />
+                <span
+                    className="text-2xl font-semibold"
+                >-</span>
+                <input
+                    type="date"
+                    className="border border-gray-300 rounded-md p-2"
+                    value={dates.endDate.toISOString().split('T')[0]}
+                    onChange={(e) => {
+                        setDates({
+                            ...dates,
+                            endDate: new Date(e.target.value)
+                        })
+                    }}
+                />
+            </div>
+
+            <div
+                className="flex justify-between items-center gap-4"
+            >
+                <span
+                    className="text-xl font-semibold"
+                >Total: {priceFormmater.format(
+                    currentSales.reduce((acc, sale) => acc + sale.total ?? 0, 0)
+                )}
+                </span>
+
+                <a
+                    href={`/print/range-report?startDate=${dates.startDate.toISOString()}&endDate=${dates.endDate.toISOString()}`}
+                    target="_blank"
+                    className="p-2 bg-blue-500 text-white rounded-md"
+                >
+                    Download
+                </a>
+            </div>
+        </article>
+    )
+}
---
+++ src/pages/api/reports/range.ts
@@ -0,0 +1,43 @@
+import { db } from "@/services/mysql";
+import type { APIContext } from "astro";
+import { z } from "astro/zod";
+import { sales } from "db/schema";
+import { and, eq, gte, lte, sql } from "drizzle-orm"; // Importing lte for less than or equal comparison
+
+const dates = z.object({
+    startDate: z.coerce.date(), // No need for coerce.date(), as we're expecting date object
+    endDate: z.coerce.date()
+})
+
+export const POST = async ({ request }: APIContext) => {
+    const body = await request.json();
+
+    const { startDate, endDate } = dates.parse(body);
+
+    console.log(startDate, endDate);
+    console.log(startDate.toISOString() === endDate.toISOString());
+
+    // Fetch sales data within the specified date range
+    const salesData =
+        startDate.toISOString() === endDate.toISOString()
+            ? await db.select().from(sales).where(
+                eq(
+                    sql`DATE(${sales.createdDate})`,
+                    startDate.toISOString().split('T')[0]
+                )
+            )
+            : await db.select().from(sales).where(
+                and(
+                    gte(
+                        sql`DATE(${sales.createdDate})`,
+                        startDate
+                    ),
+                    lte(
+                        sql`DATE(${sales.createdDate})`,
+                        endDate
+                    )
+                )
+            );
+
+    return new Response(JSON.stringify(salesData))
+}
