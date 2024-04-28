5ce757004e80f36c16d97a6038c432410350a654
KikeDeAlba <likingvirus1@gmail.com>
2024-04-28 13:14:49 -0700

    Refactor worker deletion API endpoint to handle existing sales and update delete link in workers.astro

--- src/pages/api/workers/[id]/delete.ts
+++ src/pages/api/workers/[id]/delete.ts
@@ -10,10 +10,18 @@ export const GET = async ({ params, redirect }: APIContext) => {
         return redirect('/dashboard/workers?error=Worker ID is required')
     }
 
+    try {
+        // delete worker by id
+        await db.delete(users).where(
+            eq(users.id, id)
+        ).execute()
+    } catch (error) {
+        // el error se da cuando el usuario ya ha realizado ventas y esta relacionado con la tabla de ventas
+        return redirect('/dashboard/workers?error=This worker has already made sales and cannot be deleted.')
+    }
+
     // delete worker by id
-    await db.delete(users).where(
-        eq(users.id, id)
-    ).execute()
+
 
     return redirect('/dashboard/workers')
 }
