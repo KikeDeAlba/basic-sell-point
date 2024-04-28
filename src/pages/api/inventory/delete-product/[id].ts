df28fdbc480de1305021a4a74763fa45a38f6489
KikeDeAlba <likingvirus1@gmail.com>
2024-04-28 13:13:53 -0700

    Handle error when deleting a product with existing sales in delete-product API endpoint

--- src/pages/api/inventory/delete-product/[id].ts
+++ src/pages/api/inventory/delete-product/[id].ts
@@ -6,8 +6,12 @@ import { sql } from "drizzle-orm";
 export const POST = async ({ params, redirect }: APIContext) => {
     const { id } = params
 
-    // Delete the product with the given ID from the database
-    await db.delete(products).where(sql`id = ${id}`).execute()
+    try {
+        // Delete the product with the given ID from the database
+        await db.delete(products).where(sql`id = ${id}`).execute()
+    } catch (error) {
+        return redirect('/dashboard/inventory?error=Este producto ya ha sido vendido y no puede ser eliminado.')
+    }
 
     return redirect('/dashboard/inventory')
 }
