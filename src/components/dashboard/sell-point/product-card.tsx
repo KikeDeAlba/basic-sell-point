2d80881d74accabacf578bc62e557fd050e35526
KikeDeAlba <likingvirus1@gmail.com>
2024-05-22 11:07:14 -0700

    re order buttons to add and delete products from basket

--- src/components/dashboard/sell-point/product-card.tsx
+++ src/components/dashboard/sell-point/product-card.tsx
@@ -2,65 +2,66 @@ import type { products } from "db/schema";
 import { useRef } from "react";
 import { useShopCarStore } from "stores/shop-car";
 
-
-
 export const priceFormmater = new Intl.NumberFormat("es-MX", {
-    style: "currency",
-    currency: "MXN",
+	style: "currency",
+	currency: "MXN",
 });
 
 export const ProductCard = (product: typeof products.$inferSelect) => {
-    const { description, name, price, stock, discount } = product;
+	const { description, name, price, stock, discount } = product;
 
-    const formattedPrice = priceFormmater.format(price);
-    const formattedDiscount = priceFormmater.format(discount);
-    const addProduct = useShopCarStore((state) => state.addProduct)
-    const $quantityInput = useRef<HTMLInputElement>(null)
+	const formattedPrice = priceFormmater.format(price);
+	const formattedDiscount = priceFormmater.format(discount);
+	const addProduct = useShopCarStore((state) => state.addProduct);
+	const $quantityInput = useRef<HTMLInputElement>(null);
 
-    return (
-        <form className="bg-white/30 p-4 rounded-md shadow-md h-fit"
-            onSubmit={(e) => {
-                e.preventDefault();
+	return (
+		<form
+			className="bg-white/30 p-4 rounded-md shadow-md h-fit"
+			onSubmit={(e) => {
+				e.preventDefault();
 
-                const data = new FormData(e.currentTarget);
+				const data = new FormData(e.currentTarget);
 
-                const newProduct = {
-                    ...product,
-                    quantity: Number(data.get("quantity"))
-                };
+				const newProduct = {
+					...product,
+					quantity: Number(data.get("quantity")),
+				};
 
-                if (newProduct.quantity > stock) {
-                    alert("No hay suficiente stock")
-                    return
-                }
+				if (newProduct.quantity > stock) {
+					alert("No hay suficiente stock");
+					return;
+				}
 
-                addProduct(newProduct)
+				addProduct(newProduct);
 
-                if ($quantityInput.current != null) $quantityInput.current.value = "1"
-                $quantityInput.current?.focus()
-            }}
-        >
-            <div className="flex justify-between items-center">
-                <h2 className="text-xl font-semibold">{name}</h2>
-                <span className="text-lg font-semibold">{formattedPrice}</span>
-            </div>
-            <p className="text-sm ">{description}</p>
-            <p className="text-sm ">Stock: {stock}</p>
-            <p className="text-sm ">Descuento: {formattedDiscount}</p>
-            <div className="flex justify-between items-center mt-4">
-                <input
-                    type="number"
-                    className="w-1/2 p-2 border border-gray-300 rounded-md"
-                    placeholder="Quantity"
-                    name="quantity"
-                    defaultValue={1}
-                    ref={$quantityInput}
-                />
-                <button className="px-4 py-2 bg-blue-500 text-white rounded-md">
-                    Add to cart
-                </button>
-            </div>
-        </form>
-    );
-}
-
+				if ($quantityInput.current != null) $quantityInput.current.value = "1";
+				$quantityInput.current?.focus();
+			}}
+		>
+			<div className="flex justify-between items-center">
+				<h2 className="text-xl font-semibold">{name}</h2>
+				<span className="text-lg font-semibold">{formattedPrice}</span>
+			</div>
+			<p className="text-sm ">{description}</p>
+			<p className="text-sm ">Stock: {stock}</p>
+			<p className="text-sm ">Descuento: {formattedDiscount}</p>
+			<div className="flex justify-between items-center mt-4">
+				<input
+					type="number"
+					className="w-1/2 p-2 border border-gray-300 rounded-md"
+					placeholder="Quantity"
+					name="quantity"
+					defaultValue={1}
+					ref={$quantityInput}
+				/>
+				<button
+					type="submit"
+					className="px-4 py-2 bg-blue-500 text-white rounded-md"
+				>
+					Add to cart
+				</button>
+			</div>
+		</form>
+	);
+};
--- src/components/dashboard/sell-point/ticket-product.tsx
+++ src/components/dashboard/sell-point/ticket-product.tsx
@@ -1,38 +1,55 @@
 import { useShopCarStore, type Product } from "stores/shop-car";
 
 export const TicketProduct = (product: Product) => {
-    return (
-        <li key={product.id}
-            className="flex justify-between items-center p-4 bg-white/30 rounded-md shadow-md"
-        >
-            <p>{product.name}</p>
-            <Counter id={product.id} quantity={product.quantity} stock={product.stock} />
-        </li>
-    )
-}
+	return (
+		<li
+			key={product.id}
+			className="flex justify-between items-center p-4 bg-white/30 rounded-md shadow-md"
+		>
+			<p>{product.name}</p>
+			<Counter
+				id={product.id}
+				quantity={product.quantity}
+				stock={product.stock}
+			/>
+		</li>
+	);
+};
 
-export const Counter = ({ id, quantity, stock }: { id: number, quantity: number, stock: number }) => {
-    const { addOneProduct, removeOneProduct } = useShopCarStore(state => ({
-        addOneProduct: state.addOneProduct,
-        removeOneProduct: state.removeOneProduct
-    }))
+export const Counter = ({
+	id,
+	quantity,
+	stock,
+}: { id: number; quantity: number; stock: number }) => {
+	const { addOneProduct, removeOneProduct } = useShopCarStore((state) => ({
+		addOneProduct: state.addOneProduct,
+		removeOneProduct: state.removeOneProduct,
+	}));
 
-    return (
-        <div className="flex items-center">
-            <button className="px-4 py-2 bg-blue-500 text-white rounded-md"
-                onClick={() => {
-                    if (stock < quantity) return
-                    addOneProduct(id)
-                }}
-            >
-                +
-            </button>
-            <p className={`px-4 py-2 ${stock < quantity ? 'text-red-500' : ''}`}>{quantity}</p>
-            <button className="px-4 py-2 bg-blue-500 text-white rounded-md"
-                onClick={() => removeOneProduct(id)}
-            >
-                -
-            </button>
-        </div>
-    )
-}
+	return (
+		<div className="flex items-center">
+			<button
+				className="px-4 py-2 bg-blue-500 text-white rounded-md"
+				onClick={() => removeOneProduct(id)}
+				type="button"
+			>
+				-
+			</button>
+
+			<p className={`px-4 py-2 ${stock < quantity ? "text-red-500" : ""}`}>
+				{quantity}
+			</p>
+
+			<button
+				className="px-4 py-2 bg-blue-500 text-white rounded-md"
+				onClick={() => {
+					if (stock < quantity) return;
+					addOneProduct(id);
+				}}
+				type="button"
+			>
+				+
+			</button>
+		</div>
+	);
+};
